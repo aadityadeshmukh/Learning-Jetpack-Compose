d639c334587d7377b9fd232945a913f8c6076619
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2021-06-11 12:41:40 +0530

    styling images

--- Code/app/src/main/java/com/example/hellojetpackcompose/MainActivity.kt
+++ Code/app/src/main/java/com/example/hellojetpackcompose/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +35,13 @@ fun NewsStory() {
     Column(
         modifier = Modifier.padding(16.dp)
     ){
-        Image(painter = painterResource(id = R.drawable.oreti), contentDescription = null)
+        Image(painter = painterResource(id = R.drawable.oreti),
+            contentDescription = null,
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth(),
+        contentScale = ContentScale.Crop)
+
         Text("A day in Shark fin cove")
         Text("Davenport California")
         Text("December, 2021")
--- Notes/Jetpack-Compose/Adding Images.md
+++ Notes/Jetpack-Compose/Adding Images.md
@@ -1,4 +1,13 @@
 # Adding images to app
+## Source of the image
 - Add an `Image` element using the `Image` function
 - Need to add a `painterResource`
 - Add the resource to project using Resource Manager
+
+
+## Styling the image
+- The Image function takes a modifier argument
+- Pass Modifier.height to decide the height of the element
+- Pass fillMaxWidth to specify that the image should take the width of the element it bleongs to
+- Pass contentScale to specify that the grpahic should fill the width of the column
+- contentScale.crop crops the graphic to necessary height
